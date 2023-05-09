4ba154e2e5627dc49c534fae0112ac7e21de62bd
Sevenishere <chrisprogramming53@gmail.com>
2023-05-10 05:17:37 +0530

    Progress-4

--- C-Cpp-Tutorial-video_10/01_SimpleCApp/SimpleCApp.c
+++ C-Cpp-Tutorial-video_10/01_SimpleCApp/SimpleCApp.c
@@ -4,30 +4,30 @@
 
 int32_t f(int32_t x);
 void print_fx(int32_t x, int32_t y);
-int32_t scan_x(int32_t* x);
+int32_t scan_int(int32_t* x);
+void accurate_print();
 
 int main()
 {
 	// (I) Simulate input (TODO: Replace with proper input)
 	int32_t x =0;
-	int32_t result = scan_x(&x);
+	int32_t count;
+	int32_t r0 = scan_int(&x);
+	int32_t r1 = scan_int(&count);
 	//printf("Read %i parameters, output is:",result);
 
-	int8_t check = (result != 0);// && (result != EOF);
+	// && (result != EOF);
 
-	if (check){
-		printf("Read %i parameters, output is:", result);
-		int32_t y1 = f(1 * x);
-		int32_t y2 = f(2 * x);
-		int32_t y3 = f(3 * x);
-		int32_t y4 = f(4 * x);
-		print_fx(1 * x, y1);
-		print_fx(2 * x, y2);
-		print_fx(3 * x, y3);
-		print_fx(4 * x, y4);
+	if (r0 == 1 && r1 == 1 && count>0){
+		printf("Read %i parameters, output is:", r0);
+		accurate_print(x,count);
+		
 	}
-	else if(result==EOF){
+	else if(r0==EOF || r1==EOF){
 		printf("IO Error");
+	}
+	else if (r1 == 1 && count <=0) {
+		printf("The second value must be greater than 0");
 	}
 	else {
 		printf("Invalid value");
@@ -57,9 +57,20 @@ void print_fx(int32_t x, int32_t y)
 	printf("f(%i) = %i, ", x, y);
 }
 
-int32_t scan_x(int32_t* x) {
+int32_t scan_int(int32_t* x) {
 	
 	printf("Please Enter a number:");
 	return scanf_s("%li", x);
 
+}
+
+void accurate_print(int32_t x, int32_t count) {
+
+	int32_t i = 1;
+
+	while (i <= count) {
+		int32_t y1 = f(1 * x);
+		print_fx(i * x, y1);
+		i++;
+	}
 }
